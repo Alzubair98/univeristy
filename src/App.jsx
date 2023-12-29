cd766a9639c1132c0883691df6dd028df2a5f4ea
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-12-30 00:56:43 +0300

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import "./App.css";
+import { Link, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Home from "./components/home/Home";
+import Departments from "./components/department/Departments";
 
 function App() {
   return <>here</>;
