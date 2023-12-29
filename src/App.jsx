eb78c5b214b6800d84bb1dfc14054bdba3ed227a
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-12-30 01:42:36 +0300

    add nav and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
-import { Link, Routes, Route } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Navbarr from "./components/Navbar/Navbarr.jsx";
 import Home from "./components/home/Home";
 import Departments from "./components/department/Departments";
+import About from "./components/about/About";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/Contact/Contact.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
-  return <>here</>;
+  return (
+    <div>
+      <Navbarr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Departments" element={<Departments />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
